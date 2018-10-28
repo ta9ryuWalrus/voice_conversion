33d5bfc3b1fc11ac979175b08240b7b636469a0c
ta9ryuWalrus <40373911+ta9ryuWalrus@users.noreply.github.com>
2018-10-28 22:44:30 +0900

    Delete f0makelist.sh

--- f0makelist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -e conf_f0 ];then
-	mkdir conf_f0
-fi
-
-ls data/male/data/f0/ | head -1 | sed -e 's/\.dat//' > conf_f0/train.list
-ls data/male/data/f0/ | tail -1 | sed -e 's/\.dat//' > conf_f0/eval.list
